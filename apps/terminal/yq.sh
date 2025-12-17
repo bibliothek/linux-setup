e264410ed14a112a43d0e015f04f08b893d23259
Martin Thaller <martin.thaller@uipath.com>
2025-12-17 14:46:19 +0100

    yq

--- apps/terminal/yq.sh
+++ apps/terminal/yq.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-# Set your platform variables (adjust as needed)
-VERSION=v4.2.0
 PLATFORM=linux_amd64
 
-# Download compressed binary
-wget https://github.com/mikefarah/yq/releases/download/${VERSION}/yq_${PLATFORM}.tar.gz -O - |\
-  tar xz && sudo mv yq_${PLATFORM} /usr/local/bin/yq
-
-# Or download plain binary
-wget https://github.com/mikefarah/yq/releases/download/${VERSION}/yq_${PLATFORM} -O /usr/local/bin/yq &&\
+wget https://github.com/mikefarah/yq/releases/latest/download/yq_${PLATFORM} -O /usr/local/bin/yq &&\
     chmod +x /usr/local/bin/yq
