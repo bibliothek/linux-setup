0b7f843844ed5675a4108517462260c7525ada13
Martin Thaller <thaller.martin@gmail.com>
2025-12-20 10:53:51 +0100

    sudo

--- apps/terminal/yq.sh
+++ apps/terminal/yq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PLATFORM=linux_amd64
 
-wget https://github.com/mikefarah/yq/releases/latest/download/yq_${PLATFORM} -O /usr/local/bin/yq &&\
-    chmod +x /usr/local/bin/yq
+sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_${PLATFORM} -O /usr/local/bin/yq 
+sudo chmod +x /usr/local/bin/yq
