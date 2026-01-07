bdeba5f38c877f7791e24e4ce5400b50921adbc3
Martin Thaller <martin.thaller@uipath.com>
2026-01-07 14:46:13 +0100

    brew

--- apps/prerequisites/brew.sh
+++ apps/prerequisites/brew.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-brew install gcc
+
+# Check if brew is already installed
+if command -v brew &> /dev/null; then
+  echo "Homebrew is already installed."
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+else
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  brew install gcc
+fi
