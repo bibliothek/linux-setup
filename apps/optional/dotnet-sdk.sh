318c279a21fcdd19eedda9afed49ca042dd1c14b
Martin Thaller <martin.thaller@uipath.com>
2026-05-18 14:28:00 +0200

    dotnet10

--- apps/optional/dotnet-sdk.sh
+++ apps/optional/dotnet-sdk.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo add-apt-repository -y ppa:dotnet/backports
-sudo apt update -qq
-sudo apt install -qq -y dotnet-sdk-9.0
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-10.0
+
