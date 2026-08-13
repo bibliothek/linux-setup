719264067ee2ce5359dfdac48d3a486612165444
Martin Thaller <martin.thaller@uipath.com>
2026-08-13 14:02:16 +0200

    tmux guard

--- apps/terminal/tmux.sh
+++ apps/terminal/tmux.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 brew install tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+TPM_DIR="$HOME/.tmux/plugins/tpm"
+
+# Check if tpm directory already exists
+if [ -d "$TPM_DIR" ]; then
+    echo "tpm directory already exists at $TPM_DIR"
+else
+    echo "Cloning tpm from https://github.com/tmux-plugins/tpm..."
+    mkdir -p "$(dirname "$TPM_DIR")"
+    git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+fi
